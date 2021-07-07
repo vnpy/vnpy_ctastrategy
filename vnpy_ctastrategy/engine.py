1a0643b5988cd338895b11e48da24c17b9920e7a
Gabriel Feng <fwlg1113@gmail.com>
2021-07-07 13:33:46 +0800

    Fix(CtaEngine): change joinpath method
    Thus, it can work both in windows and macos

--- vnpy_ctastrategy/engine.py
+++ vnpy_ctastrategy/engine.py
@@ -800,7 +800,7 @@ class CtaEngine(BaseEngine):
         Load strategy class from certain folder.
         """
         for suffix in ["py", "pyd", "so"]:
-            pathname: str = str(path) + f"\\*.{suffix}"
+            pathname: str = str(path.joinpath(f"*.{suffix}"))
             for filepath in glob(pathname):
                 filename: str = Path(filepath).stem
                 name: str = f"{module_name}.{filename}"
