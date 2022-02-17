f220d381b25d74399883827a216249a77aa3b172
vn.py <xiaoyou.chen@foxmail.com>
2022-02-17 11:21:42 +0800

    [Mod] 修改app_path字段类型为Path对象

--- vnpy_ctastrategy/__init__.py
+++ vnpy_ctastrategy/__init__.py
@@ -45,8 +45,8 @@ class CtaStrategyApp(BaseApp):
 
     app_name = APP_NAME
     app_module = __module__
-    app_path = str(Path(__file__).parent)
+    app_path = Path(__file__).parent
     display_name = "CTA策略"
     engine_class = CtaEngine
     widget_name = "CtaManager"
-    icon_name = str(Path(__file__).parent.joinpath("ui", "cta.ico"))
+    icon_name = str(app_path.joinpath("ui", "cta.ico"))
