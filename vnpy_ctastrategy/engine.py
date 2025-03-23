99b66fc788bc56167f720082c366af0cf566ff93
vn.py <xiaoyou.chen@foxmail.com>
2025-03-23 13:32:06 +0800

    [Mod] 注册CTA日志到日志引擎

--- vnpy_ctastrategy/engine.py
+++ vnpy_ctastrategy/engine.py
@@ -12,7 +12,7 @@ from glob import glob
 from concurrent.futures import Future
 
 from vnpy.event import Event, EventEngine
-from vnpy.trader.engine import BaseEngine, MainEngine
+from vnpy.trader.engine import BaseEngine, MainEngine, LogEngine
 from vnpy.trader.object import (
     OrderRequest,
     SubscribeRequest,
@@ -116,6 +116,9 @@ class CtaEngine(BaseEngine):
         self.event_engine.register(EVENT_TICK, self.process_tick_event)
         self.event_engine.register(EVENT_ORDER, self.process_order_event)
         self.event_engine.register(EVENT_TRADE, self.process_trade_event)
+
+        log_engine: LogEngine = self.main_engine.get_engine("log")
+        log_engine.register_log(EVENT_CTA_LOG)
 
     def init_datafeed(self) -> None:
         """
